f6e11d0662b8af067a1e8cdc7b24ca8d64f1b0e5
im <ilya.mikhailov4@gmail.com>
2023-10-01 13:34:18 +0300

    Added dotfiles

--- init.sh
+++ init.sh
@@ -9,3 +9,7 @@ apt install -y gh python3-pip
 cp -r /root/.ssh /home/im/
 chown -R im:im /home/im/.ssh
 chmod 600 /home/im/.ssh/*
+cp .gitconfig /home/im
+chown im:im /home/im/.gitconfig
+cp .vimrc /home/im
+chown im:im /home/im/.vimrc
