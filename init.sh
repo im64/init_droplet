35fb33bc22d9e12b6c03339c1e41f993418808c1
im <ilya.mikhailov4@gmail.com>
2023-10-11 17:35:31 +0300

    Added python alias

--- init.sh
+++ init.sh
@@ -7,9 +7,8 @@ usermod -aG sudo im
 apt install -y gh python3-pip
 
 cp -r /root/.ssh /home/im/
-chown -R im:im /home/im/.ssh
 chmod 600 /home/im/.ssh/*
 cp .gitconfig /home/im
-chown im:im /home/im/.gitconfig
 cp .vimrc /home/im
-chown im:im /home/im/.vimrc
+echo 'alias py=python3' >> /home/im/.bashrc
+chown -R im:im /home/im
