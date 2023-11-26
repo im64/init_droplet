42a520b7219d670d3d4f975f1525785469642b45
im64 <46765810+im64@users.noreply.github.com>
2023-11-26 17:02:58 +0200

    Update init.sh. Copy tmux.conf

--- init.sh
+++ init.sh
@@ -10,5 +10,6 @@ cp -r /root/.ssh /home/im/
 chmod 600 /home/im/.ssh/*
 cp .gitconfig /home/im
 cp .vimrc /home/im
+cp .tmux.conf /home/im
 echo 'alias py=python3' >> /home/im/.bashrc
 chown -R im:im /home/im
